6247f06a7d9b46b33260235c942c1daf8f97aaf4
Pavel <p.a.kadantsev@gmail.com>
2017-12-22 22:00:03 +0300

    Second commit - minor updated

--- coursera.py
+++ coursera.py
@@ -1,27 +1,41 @@
+import random 
 import requests
-import pprint 
 from lxml import objectify, etree
+from bs4 import BeautifulSoup
 
 
-def get_courses_urls_list():
+def get_courses_urls_list(amount):
     xml_feed_url = 'https://www.coursera.org/sitemap~www~courses.xml'
     try:
         coursera_xml = requests.get(xml_feed_url).content
+        coursera_xml.raise_for_status
     except requests.exceptions.ConnectionError as requests_error:
         print(requests_error)
     try:
         root = objectify.fromstring(coursera_xml)
-        return [url_tag.loc.text for url_tag in root.getchildren()]
-    except etree.XMLSyntaxError as etree_error:
-        print(etree_error)
+        all_courses_urls = [url_tag.loc.text for url_tag in root.getchildren()]
+        random_courses_urls = random.sample(all_courses_urls, amount)
+        return random_courses_urls
+    except etree.XMLSyntaxError, ValueError as parsing_error:
+        print(parsing_error)
 
-# def get_course_info(course_slug):
-#     pass
+def get_course_page_html(course_page_url):
+    try:
+        course_page_html = requests.get(course_page_url)
+        coursera_xml.raise_for_status
+    except requests.exceptions.ConnectionError as error:
+        print(error)
+    course_page_html.encoding = 'utf-8'
+    return course_page_html.text
+
+def get_course_name(course_page_html):
+    parsed_page = BeautifulSoup(course_page_html, html.parser)
+    
+
 
 # # def output_courses_info_to_xlsx(filepath):
 # #     pass
 
 
 if __name__ == '__main__':
-    a = get_courses_urls_list()
-    print(a)
+    get_courses_urls_list()
