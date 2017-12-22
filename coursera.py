1b01f822595667bf810ee6659cc4598b0e6eb751
Pavel Kadantsev <p.a.kadantsev@gmail.com>
2017-12-22 13:59:29 +0300

    Initial commit

--- coursera.py
+++ coursera.py
@@ -1,16 +1,27 @@
+import requests
+import pprint 
+from lxml import objectify, etree
 
 
-def get_courses_list():
-    pass
+def get_courses_urls_list():
+    xml_feed_url = 'https://www.coursera.org/sitemap~www~courses.xml'
+    try:
+        coursera_xml = requests.get(xml_feed_url).content
+    except requests.exceptions.ConnectionError as requests_error:
+        print(requests_error)
+    try:
+        root = objectify.fromstring(coursera_xml)
+        return [url_tag.loc.text for url_tag in root.getchildren()]
+    except etree.XMLSyntaxError as etree_error:
+        print(etree_error)
 
+# def get_course_info(course_slug):
+#     pass
 
-def get_course_info(course_slug):
-    pass
-
-
-def output_courses_info_to_xlsx(filepath):
-    pass
+# # def output_courses_info_to_xlsx(filepath):
+# #     pass
 
 
 if __name__ == '__main__':
-    pass
+    a = get_courses_urls_list()
+    print(a)
