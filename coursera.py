2e869462e4ccad7e8f1458545c47206ca7851290
Pavel Kadantsev <p.a.kadantsev@gmail.com>
2017-12-30 21:55:42 +0300

    Updates per checker review

--- coursera.py
+++ coursera.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 from openpyxl import Workbook
 
 
-def get_courses_urls_list(amount):
+def fetch_random_courses_urls_list(amount):
     xml_feed_url = 'https://www.coursera.org/sitemap~www~courses.xml'
     try:
         coursera_xml = requests.get(xml_feed_url)
@@ -25,35 +25,34 @@ def get_courses_urls_list(amount):
         print(parsing_error)
 
 
-def get_and_parse_course_page_html(course_page_url):
+def fetch_course_page_soup(course_page_url):
     try:
         course_page_html = requests.get(course_page_url)
-    except requests.exceptions.ConnectionError as error:
-        print(error)
+    except requests.exceptions.ConnectionError:
     course_page_html.encoding = 'utf-8'
-    parsed_page = BeautifulSoup(course_page_html.text, 'html.parser')
-    return parsed_page
+    soup_object = BeautifulSoup(course_page_html.text, 'html.parser')
+    return soup_object
 
 
-def get_course_name(parsed_page):
+def fetch_course_name(soup):
     try:
-        course_name = parsed_page.find('h1', class_='title display-3-text').text
+        course_name = soup.find('h1', class_='title display-3-text').text
     except AttributeError:
         course_name = None
     return course_name
 
 
-def get_course_language(parsed_page):
+def fetch_course_language(soup):
     try:
-        course_language = parsed_page.find('div', class_='rc-Language').text
+        course_language = soup.find('div', class_='rc-Language').text
     except AttributeError:
         course_language = 'No information'
     return course_language
 
 
-def get_course_startdate(parsed_page):
+def fetch_course_startdate(soup):
     try:
-        start_date = parsed_page.find('div', 'startdate').text
+        start_date = soup.find('div', 'startdate').text
         temporary_date_list = start_date.split(' ')
         if temporary_date_list[0].lower().startswith('start'):
             del temporary_date_list[0]
@@ -63,17 +62,17 @@ def get_course_startdate(parsed_page):
     return start_date
 
 
-def get_course_duration(parsed_page):
+def fetch_course_duration(soup):
     try:
-        duration = parsed_page.find('i', class_='cif-clock').parent.parent.contents[1].string
+        duration = soup.find('i', class_='cif-clock').parent.parent.contents[1].string
     except AttributeError:
         duration = 'No information'
     return duration
 
 
-def get_course_rating(parsed_page):
+def fetch_course_rating(soup):
     try:
-        rating = parsed_page.find('div', class_='ratings-text bt3-visible-xs').contents[0].string
+        rating = soup.find('div', class_='ratings-text bt3-visible-xs').contents[0].string
     except AttributeError:
         rating = 'No information'
     return rating
@@ -90,32 +89,31 @@ def output_courses_info_to_xlsx(filename, all_courses_data):
     workbook.save(filename)
 
 
-def fetch_script_launch_parameters():
+def get_command_line_arguments():
     parser = argparse.ArgumentParser(prog='Coursera Crawler')
-    parser.add_argument('--amount', help='How much courses to get?', type=int)
-    parser.add_argument('--filename', help='Name of output *.xlsx', type=str)
-    launch_parameters = parser.parse_args()
-    return launch_parameters
+    parser.add_argument('--amount', type=int)
+    parser.add_argument('--filename', type=str)
+    arguments = parser.parse_args()
+    return arguments
 
 
 if __name__ == '__main__':
-    launch_parameters = fetch_script_launch_parameters()
-    if launch_parameters:
-        all_courses_data_list = []
-        print('\nCollecting coureses information...')
-        urls_list = get_courses_urls_list(launch_parameters.amount)
-        for number, url in enumerate(urls_list, start=1):
-            course_data = []
-            parsed_page = get_and_parse_course_page_html(url)
-            course_data.append(number)
-            course_data.append(get_course_name(parsed_page))
-            course_data.append(get_course_language(parsed_page))
-            course_data.append(get_course_startdate(parsed_page))
-            course_data.append(get_course_duration(parsed_page))
-            course_data.append(get_course_rating(parsed_page))
-            course_data.append(url)
-            all_courses_data_list.append(course_data)
-        output_courses_info_to_xlsx(
-            launch_parameters.filename,
-            all_courses_data_list)
-        print('Done!')
+    arguments = get_command_line_arguments()
+    all_courses_data_list = []
+    print('\nCollecting coureses information...')
+    urls_list = fetch_random_courses_urls_list(arguments.amount)
+    for number, url in enumerate(urls_list, start=1):
+        course_data = []
+        soup = fetch_course_page_soup(url)
+        course_data.append(number)
+        course_data.append(fetch_course_name(soup))
+        course_data.append(fetch_course_language(soup))
+        course_data.append(fetch_course_startdate(soup))
+        course_data.append(fetch_course_duration(soup))
+        course_data.append(fetch_course_rating(soup))
+        course_data.append(url)
+        all_courses_data_list.append(course_data)
+    output_courses_info_to_xlsx(
+        arguments.filename,
+        all_courses_data_list)
+    print('Done!')
